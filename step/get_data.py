fd24cf405960024f6b9febc2c4e37565b1718394
Weizhe (Wesley) Chen <chenweiz@iu.edu>
2022-04-24 15:27:11 -0400

    Added step demo.

---
+++ step/get_data.py
@@ -0,0 +1,93 @@
+from pathlib import Path
+
+from matplotlib import pyplot as plt
+import numpy as np
+from pypolo.utilities.scalers import MinMaxScaler, StandardScaler
+
+plt.style.use(["science", "nature"])
+
+# Demo configurations
+num_trains = [30, 30, 50, 30, 30]
+eps = 0.0
+num_test = 300
+offset = 4
+xmax = 5
+noise_scale = 0.1
+
+
+# Ground-truth function
+def fn(x):
+    y = []
+    for each in x:
+        if 0.0 <= each < 1.0:
+            y.append(np.sin(10 * each) - offset)
+        elif 1.0 <= each < 2.0:
+            y.append(np.sin(10 * each) + offset)
+        elif 2.0 <= each < 3.0:
+            y.append(3 * np.sin(40 * each) - offset)
+        elif 3.0 <= each < 4.0:
+            y.append(np.sin(10 * each) + offset)
+        elif 4.0 <= each <= 5.0:
+            y.append(np.sin(10 * each) - offset)
+        else:
+            raise ValueError("Input is out of range [0, 5]")
+    return np.vstack(y)
+
+
+# Training set
+rng = np.random.RandomState(0)
+x_train = np.hstack([
+    rng.uniform(low=low + eps, high=low + 1 - eps, size=num_trains[i])
+    for low, i in enumerate(range(5))
+] + [x - 1e-2 for x in range(1, 5)] + [x + 1e-2 for x in range(1, 5)])
+x_train = x_train.reshape(-1, 1)
+y_train = fn(x_train)
+
+# Test set
+x_test = np.linspace(0, xmax, num_test).reshape(-1, 1)
+y_test = fn(x_test)
+
+# Standardize datasets
+minmaxer = MinMaxScaler(x_train, (-1, 1))
+standardizer = StandardScaler(y_train)
+x_train = minmaxer.preprocess(x_train)
+x_test = minmaxer.preprocess(x_test)
+y_train = standardizer.preprocess(y_train)
+y_test = standardizer.preprocess(y_test)
+
+# Add some observational noise to the training outputs
+y_train += noise_scale * rng.randn(len(x_train), 1)
+
+# Visualization
+width = 3.5
+height = 0.4 * 0.618 * width
+fig, ax = plt.subplots(figsize=(width, height))
+ax.set_xlim([x_test.min(), x_test.max()])
+ax.set_ylim([-2.5, 2.5])
+ax.xaxis.set_ticklabels([])
+ax.set_ylabel(f"$y$")
+ax.plot(x_test, y_test, "r-", alpha=0.6, label="Target")
+ax.scatter(x_train,
+           y_train,
+           s=3,
+           marker=".",
+           color="k",
+           alpha=0.8,
+           label="Samples")
+ax.vlines(np.arange(-0.6, 1.0, 0.4),
+          -2.5,
+          2.5,
+          colors='k',
+          linestyles='dashed',
+          alpha=0.6)
+Path("./figures/").mkdir(parents=True, exist_ok=True)
+fig.savefig(f"./figures/data.pdf", bbox_inches="tight")
+plt.close(fig)
+
+# Saving
+Path("./data/").mkdir(parents=True, exist_ok=True)
+np.save("./data/x_train.npy", x_train)
+np.save("./data/y_train.npy", y_train)
+np.save("./data/x_test.npy", x_test)
+np.save("./data/y_test.npy", y_test)
+print("Data saved to ./data/")
---
+++ step/main.py
@@ -0,0 +1,273 @@
+import torch
+from pathlib import Path
+
+import numpy as np
+import pypolo
+from matplotlib import pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+
+from parse_arguments import parse_arguments
+
+
+def rbf(dist, lengthscale):
+    """Radial Basis Function."""
+    return torch.exp(-0.5 * torch.square(dist / lengthscale))
+
+
+class LengthscaleSelectionKernel(pypolo.kernels.AK):
+
+    def __init__(self, amplitude, lengthscales, dim_input, dim_hidden,
+                 dim_output, use_softmax):
+        super().__init__(amplitude, lengthscales, dim_input, dim_hidden,
+                         dim_output, use_softmax)
+
+    def forward(self, x_1, x_2):
+        dist = torch.cdist(x_1, x_2, p=2)
+        repre1 = self.get_representations(x_1)
+        repre2 = self.get_representations(x_2)
+        cov_mat = 0.0
+        for i in range(self.num_lengthscales):
+            attention_lengthscales = torch.outer(repre1[:, i], repre2[:, i])
+            cov_mat += rbf(dist, self.lengthscales[i]) * attention_lengthscales
+        return self.amplitude * cov_mat
+
+
+class AK2(pypolo.kernels.IKernel):
+
+    def __init__(self, amplitude, lengthscales, dim_input, dim_hidden,
+                 dim_output, use_softmax):
+        super().__init__(amplitude)
+        self.lengthscales = lengthscales
+        print("AK primitive lengthscales: ", self.lengthscales)
+        self.nn_ls = pypolo.kernels.TwoHiddenLayerTanhNN(
+            dim_input, dim_hidden, dim_output, use_softmax).double()
+        self.nn_is = pypolo.kernels.TwoHiddenLayerTanhNN(
+            dim_input, dim_hidden, dim_output, use_softmax).double()
+        self.train_instance_selection = True
+
+    @property
+    def num_lengthscales(self):
+        return len(self.lengthscales)
+
+    def get_representations(self, x, detach_w=False, detach_z=False):
+        raw_w = self.nn_ls(x)
+        _w = raw_w / raw_w.norm(dim=1, keepdim=True)
+        raw_z = self.nn_is(x)
+        _z = raw_z / raw_z.norm(dim=1, keepdim=True)
+        w = _w.detach() if detach_w else _w
+        z = _z.detach() if detach_z else _z
+        return w, z
+
+    def forward(self, x_1, x_2):
+        dist = torch.cdist(x_1, x_2, p=2)
+        if self.train_instance_selection:
+            w_1, z_1 = self.get_representations(x_1, detach_w=True)
+            w_2, z_2 = self.get_representations(x_2, detach_w=True)
+        else:
+            w_1, z_1 = self.get_representations(x_1, detach_z=True)
+            w_2, z_2 = self.get_representations(x_2, detach_z=True)
+        mask = z_1 @ z_2.t()
+        cov_mat = 0.0
+        for i in range(self.num_lengthscales):
+            similarity = torch.outer(w_1[:, i], w_2[:, i])
+            cov_mat += rbf(dist, self.lengthscales[i]) * similarity
+        cov_mat *= self.amplitude * mask
+        return cov_mat
+
+
+def get_data():
+    x_train = np.load("./data/x_train.npy")
+    y_train = np.load("./data/y_train.npy")
+    x_test = np.load("./data/x_test.npy")
+    y_test = np.load("./data/y_test.npy")
+    return x_train, y_train, x_test, y_test
+
+
+def get_kernel(args):
+    if args.kernel == "rbf":
+        kernel = pypolo.kernels.RBF(
+            amplitude=args.init_amplitude,
+            lengthscale=args.init_lengthscale,
+        )
+    elif args.kernel == "lengthscale_selection":
+        kernel = LengthscaleSelectionKernel(
+            amplitude=args.init_amplitude,
+            lengthscales=np.linspace(
+                args.min_lengthscale,
+                args.max_lengthscale,
+                args.dim_output,
+            ),
+            dim_input=args.dim_input,
+            dim_hidden=args.dim_hidden,
+            dim_output=args.dim_output,
+            use_softmax=args.ak_use_softmax,
+        )
+    elif args.kernel == "ak2":
+        kernel = AK2(
+            amplitude=args.init_amplitude,
+            lengthscales=np.linspace(
+                args.min_lengthscale,
+                args.max_lengthscale,
+                args.dim_output,
+            ),
+            dim_input=args.dim_input,
+            dim_hidden=args.dim_hidden,
+            dim_output=args.dim_output,
+            use_softmax=args.ak_use_softmax,
+        )
+    else:
+        raise ValueError(f"Kernel {args.kernel} is not supported.")
+    return kernel
+
+
+def get_model(args, x_train, y_train):
+    kernel = get_kernel(args)
+    model = pypolo.models.GPR(x_train=x_train,
+                              y_train=y_train,
+                              kernel=kernel,
+                              noise=args.init_noise,
+                              lr_hyper=args.lr_hyper,
+                              lr_nn=args.lr_nn,
+                              jitter=args.jitter,
+                              is_normalized=False)
+    return model
+
+
+def optimize_model(args, model, verbose=True):
+    if args.kernel == "ak2":
+        model.optimize(num_iter=args.num_train_iter // 2, verbose=verbose)
+        model.kernel.train_instance_selection = False
+        model.optimize(num_iter=args.num_train_iter // 2, verbose=verbose)
+    else:
+        model.optimize(num_iter=args.num_train_iter, verbose=verbose)
+
+
+def plot_results(args, data, model, width=3.5 * 0.8):
+    height = 0.4 * 0.618 * width
+    fig, ax = plt.subplots(figsize=(width, height))
+    plot_data(ax, data)
+    plot_prediction(ax, data, model)
+    plot_vlines(ax)
+    save_plot(args, fig)
+
+
+def plot_data(ax, data):
+    x_train, y_train, x_test, y_test = data
+    ax.plot(x_test, y_test, "r-", alpha=0.6, label="Target")
+    ax.scatter(x_train,
+               y_train,
+               s=3,
+               marker=".",
+               color="k",
+               alpha=0.8,
+               label="Samples")
+    ax.xaxis.set_ticklabels([])
+    ax.set_xlim([x_test.min(), x_test.max()])
+    ax.set_ylim([-2.5, 2.5])
+
+
+def plot_prediction(ax, data, model):
+    x_test = data[2]
+    mean, std = model(x_test)
+    ax.plot(x_test, mean, "b-", label="Prediction", alpha=0.6)
+    ax.fill_between(x_test.ravel(),
+                    mean.ravel() - 2.0 * std.ravel(),
+                    mean.ravel() + 2.0 * std.ravel(),
+                    color="b",
+                    alpha=0.2,
+                    label="Uncertainty")
+    ax.set_ylabel(r"$y$")
+    ax.yaxis.set_major_formatter(FormatStrFormatter("%-.0f"))
+
+
+def plot_vlines(ax):
+    ax.vlines(np.arange(-0.6, 1.0, 0.4),
+              -2.5,
+              2.5,
+              colors='k',
+              linestyles='dashed',
+              alpha=0.6)
+
+
+def save_plot(args, fig, name=""):
+    Path("./figures/").mkdir(parents=True, exist_ok=True)
+    fig.savefig(f"{args.figure_dir}{args.kernel}{args.postfix}{name}.pdf",
+                bbox_inches="tight")
+    plt.close(fig)
+
+
+def plot_vectors(ax, vectors, x_test):
+    width = 2.1 / (len(x_test) - 1)
+    bars = []
+    bars.append(ax.bar(x_test, vectors[:, 0], width=width))
+    bottom = vectors[:, 0].copy()
+    for i in range(1, vectors.shape[1]):
+        bars.append(ax.bar(
+            x_test,
+            vectors[:, i],
+            width=width,
+            bottom=bottom,
+        ))
+        bottom += vectors[:, i]
+    ax.set_xlim([x_test.min(), x_test.max()])
+    ax.set_ylim([0, 1])
+
+
+def plot_weights(args, model, data, width=3.5 * 0.8):
+    x_test = data[2]
+    with torch.no_grad():
+        w = model.kernel.get_representations(torch.tensor(x_test)).numpy()
+        w /= w.sum(axis=1, keepdims=True)
+    height = 0.4 * 0.618 * width
+    fig, ax = plt.subplots(figsize=(width, height))
+    ax.xaxis.set_ticklabels([])
+    ax.set_ylabel(r"$\mathbf{w}$")
+    ax.yaxis.set_major_formatter(FormatStrFormatter("%-.0f"))
+    plot_vectors(ax, w, x_test.ravel())
+    plot_vlines(ax)
+    save_plot(args, fig, name="_weights")
+
+
+def plot_weights_and_memberships(args, model, data, width=3.5 * 0.8):
+    x_test = data[2]
+    with torch.no_grad():
+        w, z = model.kernel.get_representations(torch.tensor(x_test))
+        w, z = w.numpy(), z.numpy()
+        w /= w.sum(axis=1, keepdims=True)
+        z /= z.sum(axis=1, keepdims=True)
+    height = 0.4 * 0.618 * width
+
+    fig, ax = plt.subplots(figsize=(width, height))
+    ax.xaxis.set_ticklabels([])
+    ax.set_ylabel(r"$\mathbf{w}$")
+    ax.yaxis.set_major_formatter(FormatStrFormatter("%-.0f"))
+    plot_vectors(ax, w, x_test.ravel())
+    plot_vlines(ax)
+    save_plot(args, fig, name="_weights")
+
+    fig, ax = plt.subplots(figsize=(width, height))
+    ax.xaxis.set_ticklabels([])
+    ax.set_ylabel(r"$\mathbf{z}$")
+    ax.yaxis.set_major_formatter(FormatStrFormatter("%-.0f"))
+    plot_vectors(ax, z, x_test.ravel())
+    plot_vlines(ax)
+    save_plot(args, fig, name="_memberships")
+
+
+def main():
+    args = parse_arguments()
+    Path(args.figure_dir).mkdir(parents=True, exist_ok=True)
+    pypolo.experiments.utilities.seed_everything(args.seed)
+    data = get_data()
+    model = get_model(args, *data[:2])
+    optimize_model(args, model, verbose=True)
+    plot_results(args, data, model)
+    if args.kernel == "lengthscale_selection":
+        plot_weights(args, model, data)
+    if args.kernel == "ak2":
+        plot_weights_and_memberships(args, model, data)
+    print(f"Saved to figures/")
+
+
+if __name__ == "__main__":
+    main()
